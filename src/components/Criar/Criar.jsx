de8daff14d6b821a1a245c1c9e4d6419ccc7c6c3
Renato Tenório Lino <103604513+renatotl@users.noreply.github.com>
2022-10-01 16:33:29 -0300

    up

--- src/components/Criar/Criar.jsx
+++ src/components/Criar/Criar.jsx
@@ -88,6 +88,6 @@ export function Criar(){
     )
   
 }
-//nossa key foi colocada de forma correta é colocada onde os itens da api for varrido 
+//nossa key foi colocada de forma correta 
 
 // a mynga chave desse elemento é o props.id da lista. Ele atribui a cada elemento renderisado na tela um id uma identificação. Com isso o react vai saber quem eu estou manipulando
