0d935b6dfc887d03d74a56ffb1a112d7cadbe3c3
Renato Tenório Lino <103604513+renatotl@users.noreply.github.com>
2022-10-10 17:55:02 -0300

    feat: pasta constats com ActionMode

---
+++ src/constants/index.js
@@ -0,0 +1,7 @@
+export const ActionMode = Object.freeze({
+    DELETAR: "DELETAR",
+    ATUALIZAR: "ATUALIZAR",
+    NORMAL: "NORMAL",
+});
+
+  // ele congela um objeto e não deixa a genta mudar suas propriedades
